da4feddf0b12498797d084c770dd688837fa2652
keryann <keke2612@hotmail.fr>
2017-03-10 17:47:04 +0100

    update grille

--- app/model/Grille.rb
+++ app/model/Grille.rb
@@ -1,20 +1,19 @@
 class Grille < Model
 
-
   def generateurGrille(niveau)
-
+    return grille
   end
 
   def chargerGrille(pseudo)
-
+    return grille
   end
 
   def sauvegarderGrille(pseudo)
-
+    return true
   end
 
   def supprimerGrille(pseudo)
-
+    return true
   end
 
 end
