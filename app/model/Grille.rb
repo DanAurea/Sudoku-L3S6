0f43d1ed580f18c0bf723d050c3b3fffae27b2de
Nikuto <callof61@gmail.com>
2017-03-23 15:41:26 +0100

    Rajout dans valeurUnique pour verifié dans le block

--- app/model/Grille.rb
+++ app/model/Grille.rb
@@ -30,6 +30,12 @@ class Grille < Model
         ## Le compte rendu de l'inspection
         cr = true
 
+        ## Initialisation pour le parcours du block
+        block = Array.new()
+        x = ((num-1)*3)%9
+        y = num/3*3
+        block << grille[x..x+2][y..y+2]
+
         ## Parcours dans [0,8]
         for i in 0..8
 
@@ -49,6 +55,20 @@ class Grille < Model
                 ## La case verifié aussi
                 @grille[x][y]["unique"] = false
                 cr = false
+            end
+
+            ## le bloc est plus petit que la grille
+            if(i<3)
+                # Verification du block
+                for j in 0..2
+                    if(block[i][j] == @grille[x][y])
+                        ## La valeur unique s'errone
+                        @grille[i][j]["unique"] = false
+                        ## La case verifié aussi
+                        @grille[x][y]["unique"] = false
+                        cr = false
+                    end
+                end
             end
         end
 
