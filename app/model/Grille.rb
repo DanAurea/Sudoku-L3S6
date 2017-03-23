7a70c9a21631570e3d67d8e5e38feaf62bfc4e22
Nikuto <callof61@gmail.com>
2017-03-23 14:34:23 +0100

    Ajout de la coherence d'une valeur

--- app/model/Grille.rb
+++ app/model/Grille.rb
@@ -4,10 +4,12 @@ class Grille < Model
 
     def initialize()
         @gen = Generateur.new()
+        @grille = nil
     end
 
     def generer(niveau)
-        return @gen.generer(niveau)
+        @grille = @gen.generer(niveau)
+        return @grille
     end
 
     def charger(pseudo)
@@ -21,5 +23,36 @@ class Grille < Model
     def supprimer(pseudo)
         return true
     end
+
+    ## Retourne vrai si la cellule est bien unique
+    def valeurUnique(x ,y)
+        ## Le compte rendu de l'inspection
+        cr = true
+
+        for i in 0..9
+
+            ## Verification horizontale
+            if(@grille[x][i] == @grille[x][y])
+                ## La valeur unique s'errone
+                @grille[x][i]["unique"] = false
+                ## La case verifié aussi
+                @grille[x][y]["unique"] = false
+                cr = false
+            end
+
+            ## Verification verticale
+            if(@grille[i][y] == @grille[x][y])
+                ## La valeur unique s'errone
+                @grille[i][y]["unique"] = false
+                ## La case verifié aussi
+                @grille[x][y]["unique"] = false
+                cr = false
+            end
+        end
+
+        return cr
+
+    end
+
 
 end
