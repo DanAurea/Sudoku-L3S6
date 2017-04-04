f62512a79bcc54062e1030e3b4c5b659b68aad5f
Flambii <flambiix@gmail.com>
2017-04-04 18:18:52 +0200

    fix

--- app/view/FenetreJeuLibre.rb
+++ app/view/FenetreJeuLibre.rb
@@ -44,30 +44,30 @@ class FenetreJeuLibre < View
 	def miseEnPlace()
 		@contenu.add(@grilleDessin)
 		Fenetre::box.add(Fenetre::creerBarreMenu())
-	    Fenetre::boutonMenu_barre.signal_connect('clicked'){
-            Core::changeTo("Menu", "pseudo": @pseudo)
-        }
-	    Fenetre::boutonSauvegarder_barre.signal_connect('clicked'){
+		Fenetre::boutonMenu_barre.signal_connect('clicked'){
+			Core::changeTo("Menu", "pseudo": @pseudo)
+		}
+		Fenetre::boutonSauvegarder_barre.signal_connect('clicked'){
 
-	    }
-	    Fenetre::boutonReglage_barre.signal_connect('clicked'){
-            Core::changeTo("Reglages", "pseudo": @pseudo)
-        }
-	    Fenetre::boutonQuitter_barre.signal_connect('clicked'){
-             Fenetre::detruire()
-         }
-	    Fenetre::boutonPauseChrono_barre.signal_connect('clicked'){
+		}
+		Fenetre::boutonReglage_barre.signal_connect('clicked'){
+			Core::changeTo("Reglages", "pseudo": @pseudo)
+		}
+		Fenetre::boutonQuitter_barre.signal_connect('clicked'){
+			Fenetre::detruire()
+		}
+		Fenetre::boutonPauseChrono_barre.signal_connect('clicked'){
 
-	    }
-	    Fenetre::boutonPlayChrono_barre.signal_connect('clicked'){
+		}
+		Fenetre::boutonPlayChrono_barre.signal_connect('clicked'){
 
-	    }
-	    Fenetre::boutonAnnuler_barre.signal_connect('clicked'){
+		}
+		Fenetre::boutonAnnuler_barre.signal_connect('clicked'){
 
-	    }
-	    Fenetre::boutonRetablir_barre.signal_connect('clicked'){
-	    	
-	    }
+		}
+		Fenetre::boutonRetablir_barre.signal_connect('clicked'){
+
+		}
 		Fenetre::box.add(@contenu)
 	end
 
