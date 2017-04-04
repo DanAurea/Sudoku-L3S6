53914f9309225956d14df91ecdf42f19225ea92d
bastien bodineau <spi3086@ic2s124-05.univ-lemans.fr>
2017-04-04 15:58:45 +0200

    ajout test score et utilisateur

---
+++ test/Model/utilisateurCorrecte.rb
@@ -0,0 +1,70 @@
+# 	File description
+# 	
+# 	Author:: 		BastienMor
+# 	Developers: 	BastienMor
+# 	Version:: 		0.1
+# 	Copyright:: 	© 2017
+# 	License::   	Distributes under the same terms as Ruby
+# 	
+ 	
+# 	Class description
+# 	Classe de test unitaire de l'utilisateur.
+
+require_relative '../../app/model/Utilisateur'
+
+
+
+##
+## Creer un utilisateur et le recherche pour vérifié son existance.
+##
+## @param usr l'objet utilisateur.
+##
+## @return true si l'utilisateur a bien été creer, false sinon.
+##
+
+def checkCreation(usr)
+	usr.creerUtilisateur("toto")
+	return rechercherUtilisateur("toto")
+end
+
+
+##
+## Supprime un utilisateur et le recherche pour vérifié son inexistance.
+##
+## @param usr l'objet utilisateur.
+##
+## @return true si l'utilisateur a bien été supprimer, false sinon.
+##
+
+def checkSupression(usr)
+	supprimerUtilisateur
+	return !rechercherUtilisateur("toto")
+end
+
+
+##
+## méthode de test, génère un utilisateur et vérifi qu'il existe et qu'on peut le supprimer.
+##
+## @return true si l'utilisateur foncrionne, false sinon.
+##
+
+def utilisateurCorrecte()
+	
+
+	usr = Utilisateur.new()
+
+
+	if(!checkCreation(usr))
+		return false
+	end
+
+	if(!checkSupression(usr))
+		return false
+	end
+
+
+	puts "all green"
+	return true
+end
+
+
--- test/unitTest.rb
+++ test/unitTest.rb
@@ -10,8 +10,12 @@
 # 	Class description
 # 	Classe de test unitaire des différents modules.
 
-require_relative 'grille/grilleCorrecte'
-require 'rspec'
+require_relative "../app/core/Core.rb"
+require_relative "../app/core/Model.rb"
+require_relative 'Generateur/grilleCorrecte'
+require_relative 'Model/utilisateurCorrecte'
+require_relative 'Model/scoreCorrecte'
+#require 'rspec'
 
 
 class UnitTest
@@ -21,17 +25,61 @@ class UnitTest
 		
 	end
 
+	
+	##
+	## méthode de test du générateur de grille.
+	##
+	## @return true si le test est positif, false sinon.
+	##
+
 	def UnitTest.testerGrilleCorrecte()
 
-		RSpec.describe Generateur, "#grille" do
-			context "test" do
-				it "Generation d'une grille aleatoire" do
-					expect(grilleCorrecte()).to eq false
-				end
-			end
+		grilleCorrecte()
+
+		#RSpec.describe Generateur, "#grille" do
+		#	context "test" do
+		#		it "Generation d'une grille aleatoire" do
+		#			expect(grilleCorrecte()).to eq false
+		#		end
+		#	end
+		#end
+	end
+
+
+	##
+	## méthode de test du gestionnaire d'utilisateurs.
+	##
+	## @return true si le test est positif, false sinon.
+	##
+
+	def UnitTest.testerUtilisateurCorrecte()
+		return utilisateurCorrecte()
+	end
+
+
+	##
+	## méthode de test de la gestion des scores.
+	##
+	## @return true si le test est positif, false sinon.
+	##
+
+	def UnitTest.testerScoreCorrecte()
+		if(utilisateurCorrecte())
+			scoreCorrecte()
+		else
+			return false
 		end
 	end
+
+
 
 end
 
 puts UnitTest.testerGrilleCorrecte()
+puts UnitTest.testerUtilisateurCorrecte()
+puts UnitTest.testerScoreCorrecte()
+
+
+
+
+
