bcaf4d55abe1f85fe245c5b54d93dea4e71e197d
Flambii <flambiix@gmail.com>
2017-04-04 18:41:10 +0200

    version test apprentissage

--- app/view/FenetreApprentissage.rb
+++ app/view/FenetreApprentissage.rb
@@ -5,28 +5,87 @@
 # => Copyright::    © 2016
 # => License::      Distributes under the same terms as Ruby
 
-##
-## classe FenetreApprentissage
-##
+require Core::ROOT + "components/GrilleDessin.rb"
+require "observer"
+
 class FenetreApprentissage < View
+	include Observable
 
 	##
 	## Initialize
 	##
 	##
 	def initialize()
-		require Core::ROOT + "components/GrilleDessin.rb"
+		Header::chrono
+
+		@contenu      = Gtk::Box.new(:horizontal, 0)
+		@grilleDessin = nil
+		@scoreLabel   = nil
 	end
 
-    ##
+	##
+	## Crée le dessin de la grille
+	##
+	## @return     Self
+	##
+	def creerGrille()
+		@grilleDessin = GrilleDessin.new(@grille)
+		@grilleDessin.add_observer(self)
+		return self
+	end
+
+	##
+	## Met en place tout les éléments sur la page
+	##
+	##
+	def miseEnPlace()
+		@contenu.add(@grilleDessin)
+		Fenetre::box.add(Fenetre::creerBarreMenu())
+		miseEnPlaceBarreMenu()
+		Fenetre::box.add(@contenu)
+	end
+
+	##
+	## Met en place la barre de menu
+	##
+	## 
+	##
+	def miseEnPlaceBarreMenu()
+		Fenetre::boutonMenu_barre.signal_connect('clicked'){
+			Core::changeTo("Menu", "pseudo": @pseudo)
+		}
+		Fenetre::boutonSauvegarder_barre.signal_connect('clicked'){
+
+		}
+		Fenetre::boutonReglage_barre.signal_connect('clicked'){
+			Core::changeTo("Reglages", "pseudo": @pseudo)
+		}
+		Fenetre::boutonQuitter_barre.signal_connect('clicked'){
+			Fenetre::detruire()
+		}
+		Fenetre::boutonPauseChrono_barre.signal_connect('clicked'){
+
+		}
+		Fenetre::boutonPlayChrono_barre.signal_connect('clicked'){
+
+		}
+		Fenetre::boutonAnnuler_barre.signal_connect('clicked'){
+
+		}
+		Fenetre::boutonRetablir_barre.signal_connect('clicked'){
+
+		}
+	end
+
+	##
 	## Lance la construction du modèle de la vue. Méthode à définir dans tout les cas ! Autrement pas de rendu de la page.
 	##
 	## @return self
 	##
 	def run()
-		grilleDessin = GrilleDessin.new(@grille)
-		Fenetre::box.add(grilleDessin)
-		Fenetre::css(:chemin => "/assets/css/FenetreApprentissage.css")
+		self.creerGrille()
+		self.miseEnPlace()
+		Fenetre::css(:chemin => "/assets/css/FenetreJeuLibre.css")
 		return self
 	end
 end
