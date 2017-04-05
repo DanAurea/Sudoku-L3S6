e2a2b306c63b2ec9ce8c10ed92581f5aff22517b
Flambii <flambiix@gmail.com>
2017-04-05 10:19:20 +0200

    test

--- app/view/FenetrePseudo.rb
+++ app/view/FenetrePseudo.rb
@@ -33,7 +33,9 @@ class FenetrePseudo < View
     	@boutonQuitter = Gtk::Button.new(:label => "Quitter")
     	# VI label
     	@titreLabel = Fenetre::creerLabelType("<u>Choix du pseudo</u>", Fenetre::SIZE_TITRE)
-    	@pseudoLabel = Fenetre::creerLabelType("Votre pseudo : ", Fenetre::SIZE_PSEUDO)
+    	@pseudoLabel = Gtk::Label.new("dedede")
+        font_desc = Pango::FontDescription.new("Monospace Bold 30")
+        @pseudoLabel.override_font(font_desc)
     end
 
 	##
